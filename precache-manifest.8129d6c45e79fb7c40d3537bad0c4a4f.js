6c8c1ee57acaf59d5655f1e0dc7e21d1ba802610
Vadim Kudryashov <diklineage4@gmail.com>
2019-02-04 19:57:44 +0300

    Updates

---
+++ precache-manifest.8129d6c45e79fb7c40d3537bad0c4a4f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b435128f4aa5d3f6202e35a339643c0",
+    "url": "/static/media/Exo20-Thin.7b435128.woff2"
+  },
+  {
+    "revision": "65993ad015692f5f4741",
+    "url": "/static/css/main.7deeba92.chunk.css"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f47a89bdb6eb676e6d049309ece6718",
+    "url": "/static/media/cat.6f47a89b.png"
+  },
+  {
+    "revision": "65993ad015692f5f4741",
+    "url": "/static/js/main.65993ad0.chunk.js"
+  },
+  {
+    "revision": "f782a444c9877f04dce834988e8838cb",
+    "url": "/static/media/pattern.f782a444.png"
+  },
+  {
+    "revision": "b7f2073db20fbe66e67c7b28189c9f3b",
+    "url": "/static/media/Exo20-Thin.b7f2073d.woff"
+  },
+  {
+    "revision": "af1d9a90239a3d6b8e107072521da874",
+    "url": "/static/media/TrebuchetMS.af1d9a90.woff2"
+  },
+  {
+    "revision": "f2bb6f2202822c63c05ffbcfb9cdbb54",
+    "url": "/static/media/TrebuchetMS.f2bb6f22.woff"
+  },
+  {
+    "revision": "a7f491918c8aff43044ca1557186c284",
+    "url": "/index.html"
+  }
+];
